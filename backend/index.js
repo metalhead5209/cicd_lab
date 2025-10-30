8fbd3fe610f8d2f76c72261434e5eaef359f4509
vagrant <vagrant@ctrl1>
2025-10-30 21:26:23 +0000

    modified index file on backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-const express=require("express");const app=express();app.get("/api/health",(req,res)=>res.json({ok:true}));app.listen(3000);
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({ ok: true, message: "Server is alive and running"});
+});
+
+app.post('/api/echo', (req, res) => {
+    res.json({ received: req.body });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
