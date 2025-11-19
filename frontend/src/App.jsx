653ec59f75ef3e7848ff792a3f4a16a914c70abf
vagrant <vagrant@ctrl1>
2025-11-19 22:47:33 +0000

    pipeline test 3 - edited actual html page - changed title to "CICD-Pipeline"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ useEffect(() => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>CICD-Pipepline</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
