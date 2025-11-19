d2c87bb09e05a993c6a621c2b245e54524451fb8
vagrant <vagrant@ctrl1>
2025-11-19 23:56:13 +0000

    final pipline test (hopefully)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ useEffect(() => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>CICD-Pipepline</h1>
+      <h1>CICD-Pipepline - Final test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
